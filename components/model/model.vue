3c3081e65b5d5fdde9375f3a8789c86c7cb8eae0
MasterTang <1286545749@qq.com>
2020-11-19 21:30:50 +0800

    A model demo for interview

---
+++ components/model/model.vue
@@ -0,0 +1,109 @@
+<template>
+<div class="modal-backdrop">
+    <div class="modal" :style="mainStyles">
+        <div class="modal-header">
+            <h3>{{title}}</h3>
+        </div>
+        <div class="modal-body">
+            <p>{{content}}</p>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn-close" @click="closeSelf">关闭</button>
+            <button type="button" class="btn-confirm" @click="confirm">确认</button>
+        </div>
+    </div>
+</div>
+</template>
+
+
+<script>
+export default {
+    name: 'Modal',
+    props: {
+        title:{
+            type:String,
+            default:'我是标题'
+        },
+        content:{
+            type:String,
+            default:'我是正文内容'
+        }
+    },
+    data() {
+        return {}
+    },
+    methods: {
+        closeSelf() {
+            this.$emit("closeModel");
+        },
+        confirm(){
+            this.$emit("confirmModel");
+        }
+    }
+}
+</script>
+<style scoped>
+.modal-backdrop {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, .3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.modal {
+    background-color: #fff;
+    box-shadow: 2px 2px 20px 1px;
+    overflow-x: auto;
+    display: flex;
+    flex-direction: column;
+    border-radius: 16px;
+    width: 700px;
+}
+
+.modal-header {
+    border-bottom: 1px solid #eee;
+    color: #313131;
+    justify-content: space-between;
+    padding: 15px;
+    display: flex;
+}
+
+.modal-footer {
+    border-top: 1px solid #eee;
+    justify-content: flex-end;
+    padding: 15px;
+    display: flex;
+}
+
+.modal-body {
+    position: relative;
+    padding: 20px 10px;
+}
+
+.btn-close,
+.btn-confirm {
+    border-radius: 8px;
+    margin-left: 16px;
+    width: 56px;
+    height: 36px;
+    border: none;
+    cursor: pointer;
+}
+
+.btn-close {
+    outline: none;
+    color: #313131;
+    background-color: transparent;
+}
+
+.btn-confirm {
+    outline: none;
+    color: #fff;
+    background-color: #2d8cf0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,87 @@
+<template>
+<div class="container">
+    <button @click="toggleModal">打开Modal对话框</button>
+    <transition name="fade">
+        <model v-show="showModal" @closeModel="closeModel" @confirmModel="confirmModel" :title="modelTitle" :content="modelContent" />
+    </transition>
+</div>
+</template>
+
+<script>
+import Model from '../components/model/model'
+export default {
+    name: 'index',
+    data() {
+        return {
+            showModal: false,
+            modelTitle: '我是一个标题',
+            modelContent: '我是正文内容'
+        }
+    },
+    components: {
+        Model
+    },
+    methods: {
+        toggleModal: function () {
+            this.showModal = !this.showModal;
+        },
+        closeModel: function () {
+            this.showModal = !this.showModal;
+            window.alert('您取消了');
+        },
+        confirmModel() {
+            this.showModal = !this.showModal;
+            window.alert('您确定了')
+        }
+    }
+
+}
+</script>
+
+<style>
+.container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
+
+.title {
+    font-family:
+        'Quicksand',
+        'Source Sans Pro',
+        -apple-system,
+        BlinkMacSystemFont,
+        'Segoe UI',
+        Roboto,
+        'Helvetica Neue',
+        Arial,
+        sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+}
+
+.subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+}
+
+.links {
+    padding-top: 15px;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
